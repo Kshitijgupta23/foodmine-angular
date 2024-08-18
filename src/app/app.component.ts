5ee50ffb56ac3ac0205e0acda4f8b360cb615987
Kshitij Gupta <kshitij.gupta2@spglobal.com>
2024-08-19 03:21:23 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from './home/home.component';
+import { RatingModule } from 'ng-starrating';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule, HeaderComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FoodService } from '../services/food/food.service';
+import { Food } from '../shared/models/food';
+import { RatingModule } from 'ng-starrating';
+import { ActivatedRoute } from '@angular/router';
+import { SearchComponent } from "../search/search.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, SearchComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit {
+  foods: Food[] = [];
+
+  constructor(private foodService: FoodService, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      if (params['searchTerm']) {
+        this.foods = this.foodService.getAll().filter(food => 
+          food.name.toLowerCase().includes(params['searchTerm'].toLowerCase())
+        );
+      } else {
+        this.foods = this.foodService.getAll();
+      }
+    });
+  }
+}
